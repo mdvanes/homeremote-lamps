7c9cc46df544a7f843203e8a9df01163fbfda055
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-12-28 10:54:49 +0100

    Convert lamp to styled component

--- client/src/Lamp.tsx
+++ client/src/Lamp.tsx
@@ -1,16 +1,29 @@
-import React, {SFC} from 'react';
-import './Lamp.css';
+import React, {FC} from 'react';
+import styled from 'styled-components';
 
 interface IProps {
   name: string;
   isOn: boolean;
 }
 
-const Lamp: SFC<IProps> = ({name, isOn}) => (
-  <div className="lamp" style={{
-    backgroundColor: isOn ? 'darkgreen' : 'darkred',
-  }}>
+const LampButton:any = styled.button`
+  background-color: ${(props:any) => props.isOn ? 'darkgreen' : 'darkred'};
+  border: 1px solid black;
+  border-radius: 3px;
+  color: #ddd;
+  cursor: pointer;
+  display: inline-block;
+  margin: 0.2rem;
+  padding: 0.3rem;
+  
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+const Lamp: FC<IProps> = ({name, isOn}) => (
+  <LampButton isOn={isOn}>
     {name}
-  </div>);
+  </LampButton>);
 
 export default Lamp;
