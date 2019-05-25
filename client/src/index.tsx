ec3e564814de8201750f7c65725b37e1c70bd4e5
M.D. van Es <es@mdworld.nl>
2019-05-25 08:58:17 +0200

    Fix initial state

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,16 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ApolloClient from 'apollo-boost';
+import ApolloClient, {InMemoryCache} from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import {defaults, resolvers} from './resolvers'
 
+const cache = new InMemoryCache();
+
+cache.writeData({
+  data: defaults,
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  clientState: {
-    defaults,
-    resolvers
+  resolvers: {
+    Mutation: {
+
+    }
   },
+  cache
   // Does not work with Apollo Boost?
   // connectToDevTools: true
 });
