cef068062aa4c934acb504ee63af1bfebed29d03
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2019-05-26 17:55:03 +0200

    Clean up

--- client/src/Components/VendorLayersControl.tsx
+++ client/src/Components/VendorLayersControl.tsx
@@ -1,10 +1,21 @@
 import React, { FC } from "react";
-import {Map, TileLayer, Marker, Popup, LayersControl, LayerGroup, Circle, FeatureGroup, Rectangle} from "react-leaflet";
+import {
+  TileLayer,
+  Marker,
+  Popup,
+  LayersControl,
+  LayerGroup,
+  Circle,
+  FeatureGroup,
+  Rectangle
+} from "react-leaflet";
 const { BaseLayer, Overlay } = LayersControl;
 
 const position: [number, number] = [52.154, 5.295];
-const zoom = 8;
-const rectangle: [number, number][] = [[52.05668, 5.11049], [51.70266, 5.28726]];
+const rectangle: [number, number][] = [
+  [52.05668, 5.11049],
+  [51.70266, 5.28726]
+];
 
 // NL 8/52.172/5.738
 // Nieuwegein 52.05668/5.11049 17
@@ -14,54 +25,53 @@ const rectangle: [number, number][] = [[52.05668, 5.11049], [51.70266, 5.28726]]
 
 const VendorLayersControl: FC = () => {
   return (
-        <LayersControl position="topright">
-          <BaseLayer checked name="OpenStreetMap.Mapnik">
-            <TileLayer
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <LayersControl position="topright">
+      <BaseLayer checked name="OpenStreetMap.Mapnik">
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </BaseLayer>
+      <BaseLayer name="OpenStreetMap.BlackAndWhite">
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+        />
+      </BaseLayer>
+      <Overlay name="Marker with popup">
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Overlay>
+      <Overlay checked name="Layer group with circles">
+        <LayerGroup>
+          <Circle center={position} fillColor="blue" radius={200} />
+          <Circle
+            center={position}
+            fillColor="red"
+            radius={100}
+            stroke={false}
+          />
+          <LayerGroup>
+            <Circle
+              center={[51.51, -0.08]}
+              color="green"
+              fillColor="green"
+              radius={100}
             />
-          </BaseLayer>
-          <BaseLayer name="OpenStreetMap.BlackAndWhite">
-            <TileLayer
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-            />
-          </BaseLayer>
-          <Overlay name="Marker with popup">
-            <Marker position={position}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </Overlay>
-          <Overlay checked name="Layer group with circles">
-            <LayerGroup>
-              <Circle center={position} fillColor="blue" radius={200} />
-              <Circle
-                center={position}
-                fillColor="red"
-                radius={100}
-                stroke={false}
-              />
-              <LayerGroup>
-                <Circle
-                  center={[51.51, -0.08]}
-                  color="green"
-                  fillColor="green"
-                  radius={100}
-                />
-              </LayerGroup>
-            </LayerGroup>
-          </Overlay>
-          <Overlay name="Feature group">
-            <FeatureGroup color="purple">
-              <Popup>Popup in FeatureGroup</Popup>
-              <Circle center={[51.51, -0.06]} radius={200} />
-              <Rectangle bounds={rectangle} />
-            </FeatureGroup>
-          </Overlay>
-        </LayersControl>
-
+          </LayerGroup>
+        </LayerGroup>
+      </Overlay>
+      <Overlay name="Feature group">
+        <FeatureGroup color="purple">
+          <Popup>Popup in FeatureGroup</Popup>
+          <Circle center={[51.51, -0.06]} radius={200} />
+          <Rectangle bounds={rectangle} />
+        </FeatureGroup>
+      </Overlay>
+    </LayersControl>
   );
 };
 
--- client/src/Resolvers/Query/vendors.ts
+++ client/src/Resolvers/Query/vendors.ts
@@ -1,30 +1,33 @@
 import { Resolver } from "../Resolver.type";
-import { Vendor } from "../../Components/DeviceList";
+import {Country, Vendor} from "../../Components/DeviceList";
 
-const vendors: Resolver = (_, variables, { cache, getCacheKey }): Vendor[] => {
+const vendors: Resolver = (_, variables , { cache, getCacheKey }): Vendor[] => {
   console.log("in Vendor Resolver", variables);
-  return [
-    {
-      __typename: "Vendor",
-      name: "EddShop",
-      stock: ["Switcheroo", "IrPlug"],
-      location: {
-        __typename: "Location",
-        lat: 1.23456,
-        lon: 1.24566
+  if(variables && variables.countryCode === Country.NL) {
+    return [
+      {
+        __typename: "Vendor",
+        name: "EddShop",
+        stock: ["Switcheroo", "IrPlug"],
+        location: {
+          __typename: "Location",
+          lat: 1.23456,
+          lon: 1.24566
+        }
+      },
+      {
+        __typename: "Vendor",
+        name: "HotRed",
+        stock: ["Switcheroo", "IrPlug"],
+        location: {
+          __typename: "Location",
+          lat: 1.23456,
+          lon: 1.24566
+        }
       }
-    },
-    {
-      __typename: "Vendor",
-      name: "HotRed",
-      stock: ["Switcheroo", "IrPlug"],
-      location: {
-        __typename: "Location",
-        lat: 1.23456,
-        lon: 1.24566
-      }
-    }
-  ];
+    ];
+  }
+  return [];
 };
 
 export default vendors;
