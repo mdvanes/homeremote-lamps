3cbbe425f4d9fa26aa8f0df5be6c8b5d21fa4cd8
M.D. van Es <es@mdworld.nl>
2019-05-26 14:45:47 +0200

    Add Vendor Map

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
-import React, {FC, Fragment} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LampList from './LampList';
-import LocalStateDirectWriteButton from './LocalStateDirectWriteButton';
-import LocalStateMutationToggle from './LocalStateMutationToggle';
-import {gql} from 'apollo-boost';
-import {Query} from "react-apollo";
+import React, { FC, Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LampList from "./LampList";
+import LocalStateDirectWriteButton from "./LocalStateDirectWriteButton";
+import LocalStateMutationToggle from "./LocalStateMutationToggle";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 import DeviceList from "./Components/DeviceList";
+import VendorMap from "./Components/VendorMap";
 
 const GET_ANIMATION_ITEMS = gql`
   {
     animationItems @client {
-      id,
+      id
       isAnimating
     }
   }
@@ -19,36 +20,38 @@ const GET_ANIMATION_ITEMS = gql`
 
 const App: FC = () => (
   <div className="App">
-      <DeviceList title="Devices" />
+    <DeviceList title="Devices" />
+    <VendorMap />
     {/*{({ data: { animationItems}}:any) => {
         console.log(animationItems);
         return (<div></div>)
       }}*/}
     <Query query={GET_ANIMATION_ITEMS}>
-      {({ data: { animationItems}}:any) => (
-        animationItems.map((item:any) => {
-            const className = `App-logo ${item.isAnimating ? 'on' : 'off'}`;
-            return (
-              <Fragment key={`fr${item.id}`}>
-                <header className="App-header" style={
-                  {
-                    minHeight: 'auto'
-                  }
-                }>
-                  <img src={logo} className={className} alt="logo" />
-                </header>
-                <LocalStateMutationToggle {...item} />
-              </Fragment>
-            )
-          })
-      )}
+      {({ data: { animationItems } }: any) =>
+        animationItems.map((item: any) => {
+          const className = `App-logo ${item.isAnimating ? "on" : "off"}`;
+          return (
+            <Fragment key={`fr${item.id}`}>
+              <header
+                className="App-header"
+                style={{
+                  minHeight: "auto"
+                }}
+              >
+                <img src={logo} className={className} alt="logo" />
+              </header>
+              <LocalStateMutationToggle {...item} />
+            </Fragment>
+          );
+        })
+      }
     </Query>
 
-    <hr/>
+    <hr />
     <p>These cards share the same state store</p>
     <LocalStateDirectWriteButton />
     <LocalStateDirectWriteButton />
-    <hr/>
+    <hr />
     <LampList />
   </div>
 );
